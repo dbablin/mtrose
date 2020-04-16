af30b1305759be609bb79bd90b6b00945e74c3c8
Daniel Bablinskas <dbablin@gmail.com>
2020-04-15 18:15:41 -0700

    build ready

--- src/shared/recreationItems.js
+++ src/shared/recreationItems.js
@@ -2,28 +2,28 @@ export const RECREATIONITEMS = [
     {
         id: 0,
         name: "Hike Mount Rose",
-        image: "/assets/images/hikeMountRose.jpg",
+        image: "/../../assets/images/hikeMountRose.jpg",
         featured: true,
         description: "Spend the day trekking 8-miles to the top of Mount Rose and back. "
     },
     {
         id: 1,
         name: "Ski Rose",
-        image: "/assets/images/skiRose.jpg",
+        image: "/../../assets/images/skiRose.jpg",
         featured: false,
         description: "Hit the slopes at Mount Rose Ski Resort, almost 9000 feet at the base!"
     },
     {
         id: 3,
         name: "Lake Tahoe",
-        image: "/assets/images/boatTour.jpg",
+        image: "/../../assets/images/boatTour.jpg",
         featured: false,
         description: "Book a day trip around Lake Tahoe, the most beautiful lake in the West!"
     },
     {
         id: 4,
         name: "Mount Biking",
-        image: "/assets/images/mtnBiking.jpg",
+        image: "/../../assets/images/mtnBiking.jpg",
         featured: false,
         description: "Explore the most gnarly bike rides in the Tahoe region with a group of dedicated riders."
     }
--- src/shared/rooms.js
+++ src/shared/rooms.js
@@ -2,7 +2,7 @@ export const ROOMS = [
     {
         id: 0,
         name: "Mountain Views",
-        image: "/assets/images/mtnViews.jpg",
+        image: "/../../assets/images/mtnViews.jpg",
         cost: 100,
         featured: false,
         description: "Our mountain view rooms in the main lodge all feature a king bed, built-in gas fireplace, and mountain views."
@@ -10,7 +10,7 @@ export const ROOMS = [
     {
         id: 1,
         name:"Lakeside",
-        image: "/assets/images/lakeside.jpg",
+        image: "/../../assets/images/lakeside.jpg",
         cost: 200,
         featured: false,
         description: "Our rooms facing Lake Tahoe are located in the main lodge, and feature two queen beds and a kitchenette."
@@ -18,7 +18,7 @@ export const ROOMS = [
     {
         id: 2,
         name:"Hike-in Cabins",
-        image: "/assets/images/hikeInCabin.jpg",
+        image: "/../../assets/images/hikeInCabin.jpg",
         cost: 50,
         featured: true,
         description: "For the adventurous sort, there are private cabins deep in the woods, away from the crowds. Booking is seasonal."
