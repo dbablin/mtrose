ea9bde442cb69b8a12af96b1954f1940dfb6327d
Daniel Bablinskas <dbablin@gmail.com>
2020-04-18 10:13:48 -0700

    message

--- src/shared/amenities.js
+++ src/shared/amenities.js
@@ -2,21 +2,21 @@ export const AMENITIES = [
     {
         id: 0,
         name: "Award-winning breakfast",
-        image: "/../../assets/images/breakfast.jpg",
+        image: "/assets/images/breakfast.jpg",
         featured: true,
         description: "Enjoy a full North American breakfast, including French toast with maple syrup, bacon, eggs, and sausage."
     },
     {
         id: 1,
         name: "Brewery at the lodge",
-        image: "/../../assets/images/brewery.jpg",
+        image: "/assets/images/brewery.jpg",
         featured: false,
         description: "Enjoy locally brewed beers and a variety of tapas and entrees at our main lodge."
     },
     {
         id: 3,
         name: "Indoor spa and sauna",
-        image: "/../../assets/images/saunaSpa.jpg",
+        image: "/assets/images/saunaSpa.jpg",
         featured: false,
         description: "All year long, warm up in the spa and sauna!"
     }
