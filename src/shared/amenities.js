8582c5bfff89097dca166f5b5a002749a82daa31
Daniel Bablinskas <dbablin@gmail.com>
2020-04-20 16:55:52 -0700

    heroku

--- src/shared/amenities.js
+++ src/shared/amenities.js
@@ -2,14 +2,14 @@ export const AMENITIES = [
     {
         id: 0,
         name: "Award-winning breakfast",
-        image: "../assets/images/breakfast.jpg",
+        image: "/assets/images/breakfast.jpg",
         featured: true,
         description: "Enjoy a full North American breakfast, including French toast with maple syrup, bacon, eggs, and sausage."
     },
     {
         id: 1,
         name: "Brewery at the lodge",
-        image: "./assets/images/brewery.jpg",
+        image: "/assets/images/brewery.jpg",
         featured: false,
         description: "Enjoy locally brewed beers and a variety of tapas and entrees at our main lodge."
     },
