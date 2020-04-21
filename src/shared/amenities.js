071dd7d8c20bd75131967a86bc79317dc68b34e4
Daniel Bablinskas <dbablin@gmail.com>
2020-04-20 17:22:09 -0700

    working

--- src/shared/amenities.js
+++ src/shared/amenities.js
@@ -9,14 +9,14 @@ export const AMENITIES = [
     {
         id: 1,
         name: "Brewery at the lodge",
-        image: "/assets/images/brewery.jpg",
+        image: "/mtrose/assets/images/brewery.jpg",
         featured: false,
         description: "Enjoy locally brewed beers and a variety of tapas and entrees at our main lodge."
     },
     {
         id: 3,
         name: "Indoor spa and sauna",
-        image: "/assets/images/saunaSpa.jpg",
+        image: "/mtrose/assets/images/saunaSpa.jpg",
         featured: false,
         description: "All year long, warm up in the spa and sauna!"
     }
--- src/shared/carouselItems.js
+++ src/shared/carouselItems.js
@@ -2,19 +2,19 @@ export const CAROUSELITEMS = [
     {
         id: 0,
         name: "Mountain Views",
-        image: "/assets/images/mountainviews2.jpg",
+        image: "/mtrose/assets/images/mountainviews2.jpg",
         description: "Our standard rooms all feature a king bed, built-in gas fireplace, and mountain views."
     },
     {
         id: 1,
         name: "Remote Cabins",
-        image: "/assets/images/remotecabin.jpg",
+        image: "/mtrose/assets/images/remotecabin.jpg",
         description: "For the adventurous sort, there are private cabins deep in the woods, away from the crowds."
     },
     {
         id: 2,
         name: "Rustic Lodge",
-        image: "/assets/images/rusticlodge.jpg",
+        image: "/mtrose/assets/images/rusticlodge.jpg",
         description: "Whether you're returning from skiing or a day of soaking up the sun, a fresh brew always sounds right."
     }
 ];
